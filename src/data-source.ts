b86bf8c8fe1e6725fa86d4e7b0fea14992a01c04
cuttleman <silluat11@gmail.com>
2023-07-06 02:06:57 +0900

    with Dockerfile

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
-    
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+  port: 5432,
+  host: process.env.DB_HOST,
+  username: "test",
+  password: "test",
+  database: "test",
+});
